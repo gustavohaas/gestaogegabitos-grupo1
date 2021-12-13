738c797c693b1e194018bc75909bdac21b13472d
André Martins <andresmartins910@gmail.com>
2021-12-13 15:14:40 -0300

    feature: home page

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    font-family: monospace;
+    margin-bottom: 4rem;
+  }
+
+  div {
+    display: flex;
+    width: 300px;
+  }
+
+  button {
+    font-weight: bold;
+  }
+
+  button + button {
+    margin-left: 10px;
+  }
+`;
