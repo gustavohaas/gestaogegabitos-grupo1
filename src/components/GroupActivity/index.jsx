6eb2d86eea002c0ae6289a26d38597c0356c467d
Gustavo B. Haas <gbhaas97@gmail.com>
2021-12-15 09:49:17 -0300

    feat: creating activity creation

--- src/components/GroupActivity/index.jsx
+++ src/components/GroupActivity/index.jsx
@@ -1,7 +1,12 @@
 import { useContext, useEffect } from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 import { StyleList, StyleListContainer } from "./style";
 import { GroupsActivitiesContext } from "../../providers/GroupsActivities";
 import Checkbox from "../Checkbox";
+import Input from "../Input";
+import Button from "../Button";
 
 
 export const ActivityList = () => {
@@ -34,6 +39,36 @@ export const ActiviryPopUp = () => {
 
     const { createActivity } = useContext(GroupsActivitiesContext);
 
-    
+    const activitySchema = yup.object().shape({
+        title: yup.string().required("Campo obrigatório"),
+        realization_time_date: yup.date().min(new Date()).required("Campo obrigatório"),
+        realization_time: yup.
+    })
+
+    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(activitySchema) });
+
+    const handleActivity = (data, groupID = 422) => {
+        const time = realization_time_date
+        
+        createActivity(data)
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <label for='title' >
+                Título <span>{errors.title?.message}</span>
+            </label>
+            <Input name='title' register={register} />
+            <label for='realization_time_date' >
+                Data de realização <span>{errors.realization_time_date?.message}</span>
+            </label>
+            <Input name='realization_time_date' register={register} />
+            <label for='realization_time' >
+                Horário de realização <span>{errors.realization_time?.message}</span>
+            </label>
+            <Input name='realization_time' register={register} />
+            <Button type="submit">Cadastrar</Button>
+        </form>
+    );
 
 };
